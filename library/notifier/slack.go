078c9a2802b8c7f30c34df2a17e158b3d48a2bfb
Rodion <rodion.lim@hotmail.com>
2022-02-21 09:48:37 +0800

    feat(slack): add logging

--- library/notifier/slack.go
+++ library/notifier/slack.go
@@ -1,21 +1,23 @@
 package notifier
 
 import (
-	"fmt"
+	"context"
 	"os"
 
+	"github.com/rodionlim/carousell/library/log"
 	"github.com/slack-go/slack"
 )
 
 type Slack struct {
-	client  *slack.Client
-	channel string // Default channel to post messages to if not specified
+	client *slack.Client
 }
 
 func NewSlacker() *Slack {
+	logger := log.Ctx(context.Background())
 	token, exists := os.LookupEnv("SLACK_ACCESS_TOKEN")
 	if !exists {
-		fmt.Println("Invalid slack access token. Please set \"SLACK_ACCESS_TOKEN\" variable")
+		logger.Error("Invalid slack access token. Please set \"SLACK_ACCESS_TOKEN\" variable")
+		os.Exit(1)
 	}
 
 	return &Slack{
@@ -26,6 +28,7 @@ func NewSlacker() *Slack {
 // Notify acts as a wrapper to send basic slack messages.
 // It requires the env SLACK_ACCESS_TOKEN to be set.
 func (s *Slack) Notify(channelID string, msg string, attachment *slack.Attachment) {
+	logger := log.Ctx(context.Background())
 	msgOptions := []slack.MsgOption{
 		slack.MsgOptionText(msg, false),
 		slack.MsgOptionAsUser(true), // Add this if you want that the bot would post message as a user, otherwise it will send response using the default slackbot
@@ -38,8 +41,8 @@ func (s *Slack) Notify(channelID string, msg string, attachment *slack.Attachmen
 		msgOptions...,
 	)
 	if err != nil {
-		fmt.Printf("%s\n", err)
+		logger.Errorf("%s\n", err)
 		return
 	}
-	fmt.Printf("Message successfully sent to channel %s at %s\n", channelID, timestamp)
+	logger.Infof("Message successfully sent to channel %s at %s\n", channelID, timestamp)
 }
